b082d0ec7b69dc0be7ba5768149b15a1321eedb9
blobrus <bhines7@gmail.com>
2022-06-09 19:29:42 -0500

    Color changes
    Successfully integrated custom file to override default bootstrap colors.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+/*Bootstrap is imported as part of this*/
+import '../scss/custom.scss'
 
 createApp(App).mount('#app')
